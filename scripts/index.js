f97ff0f14597828d5da9acaf0923c426c9a03fa3
ana-STAS-iia <kalach.03@bk.ru>
2024-03-14 17:17:52 +0500

    В первый раз заливаю

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+function createCard(link, name, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardList.append(cardElement);
+    
+    cardDeleteButton.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach(function(card) {
+    createCard(card.link, card.name, deleteCard);
+});
